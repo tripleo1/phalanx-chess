b181835487545c40ce6ff44cf5abc3ad913a8978
dusando <dusando@9b3bb02a-7510-4cd5-ae98-52c51b953421>
2014-04-12 16:31:45 +0000

    tuned killers

--- phalanx/killers.c
+++ phalanx/killers.c
@@ -78,32 +78,53 @@ for( i=0; i!=n; i++ )
 
 	if( m1->value != CHECKMATE )
 	{
+		/* mopv - moving piece value */
+		int mopv = Values[ m1->in2a >> 4 ];
+
 		if( m1->in2 ) /* capture */
 		{
+			m1->value += Values[ m1->in2>>4 ];
+
 			/* capture of last moved piece */
 			if( m1->to == G[Counter-1].m.to ) m1->value += 150;
 
-			m1->value += Values[ m1->in2>>4 ];
 			if( Color==WHITE )
-			{ if( P[m1->to]&0xFF00 )
-			  m1->value -= ( Values[ m1->in1>>4 ] >> 2 );
-			  else m1->value += 100; }
+			{
+			  if( P[m1->to]&0xFF00 )
+			  m1->value -= ( mopv / 4 );
+			  else m1->value += 150;
+			}
 			else
-			{ if( P[m1->to]&0x00FF )
-			  m1->value -= ( Values[ m1->in1>>4 ] >> 2 );
-			  else m1->value += 100; }
+			{
+			  if( P[m1->to]&0x00FF )
+			  m1->value -= ( mopv / 4 );
+			  else m1->value += 150;
+			}
 
 			if( m1->value < 30 ) m1->value = 30;
 
 			if( Depth > 200 )
+			{
 			  m1->value += see( B, m1->from, m1->to );
+			  if(m1->value<0) m1->value=20;
+			}
 		}
-
-		/* leaving attacked square */
-		if( Color==WHITE )
-		{ if(P[m1->from]&0xFF00) m1->value += 50; }
-		else
-		{ if(P[m1->from]&0x00FF) m1->value += 50; }
+		/* non captures - look at the power tables */
+		else if( mopv>P_VALUE )
+		{
+		  if( Color==WHITE )
+		  {
+			/* leaving attacked square */
+			if(P[m1->from]&0xFF00) m1->value += mopv/20;
+			/* going to attacked square */
+			if(P[m1->to]&0xFF00) m1->value -= mopv/15;
+		  }
+		  else
+		  {
+			if(P[m1->from]&0x00FF) m1->value += mopv/20;
+			if(P[m1->to]&0x00FF) m1->value -= mopv/15;
+		  }
+		}
 
 		if( m1->in1 != m1->in2a ) /* pawn promotion */
 			m1->value += Values[ m1->in2a>>4 ] - P_VALUE;
